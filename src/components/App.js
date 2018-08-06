9d3235c14bc9a9945e51065fce298313b6572e7b
paderinandrey <paderinandrey2011@gmail.com>
2018-08-06 23:18:10 +0300

    add router helper

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import CatalogPage from './CatalogPage';
 import { CartProvider } from '../containers/CartContainer';
 import Cart from './Cart';
---
+++ src/helpers/routes.js
@@ -0,0 +1,5 @@
+export const rootPath = () => '/';
+export const productsPath = () => '/products';
+export const productPath = (id = ':id') => `/products/${id}`;
+export const cartPath = () => '/cart';
+export const contactsPath = () => '/contacts';
