baf7b02cd688093663ec26955be53c77a0f3e39c
paderinandrey <paderinandrey2011@gmail.com>
2018-08-09 03:06:40 +0300

    empty cart notification

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import CatalogPage from './CatalogPage';
-import Cart from './Cart';
 import Menu from './Menu';
+import CartButton from './CartButton';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const { history } = this.props;
+    history.listen((location, action) => {
+      if (location.state && location.state.message) {
+        alert(location.state.message);
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         <Menu />
-        <Cart />
+        <CartButton />
         <CatalogPage />
       </div>
     );
--- src/components/CatalogPage.js
+++ src/components/CatalogPage.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Catalog from './Catalog';
-import { CartProvider } from '../containers/CartContainer';
 import products from '../constants/Products';
 
 export default class CatalogPage extends React.Component {
+
   render() {
-    return (
-      <CartProvider>
-        <Catalog products={products || []} />
-      </CartProvider>
-    );
+    return <Catalog products={products || []} />;
   }
 }
