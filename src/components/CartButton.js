9f7e0275e0e9ff3d356dd32dcbf191cc84197333
paderinandrey <paderinandrey2011@gmail.com>
2018-08-09 03:03:02 +0300

    Cart button

---
+++ src/components/CartButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { CartConsumer } from '../containers/CartContainer';
+import { rootPath, cartPath } from '../helpers/routes';
+import { withRouter } from 'react-router-dom';
+
+class cartButton extends React.Component {
+  handleDrop(ev, addToCart) {
+    const { product } = JSON.parse(ev.dataTransfer.getData('selectedProduct'));
+    addToCart(product);
+  }
+
+  handleClick(e, cart) {
+    if (cart.length > 0) {
+      this.props.history.push(cartPath());
+    }
+    else {
+      this.props.history.push(rootPath(), { message: "Your cart is currently empty. Please add some products to cart." });
+    }
+  }
+
+  render() {
+    return (
+      <CartConsumer>
+        {
+          ({cart, addToCart}) => (
+            <button
+              onClick={(e) => this.handleClick(e, cart)}
+              onDrop={(e) => this.handleDrop(e, addToCart)}
+              onDragOver={(e) => e.preventDefault()}>
+              Cart ({cart.length})
+            </button>
+          )
+        }
+      </CartConsumer>
+    );
+  }
+}
+
+export default withRouter(cartButton);
