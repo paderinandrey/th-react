7e940fa2ca12d27514afd39758d41fcb044be858
paderinandrey <paderinandrey2011@gmail.com>
2018-08-26 18:57:05 +0300

    add config client

---
+++ config.js
@@ -0,0 +1,2 @@
+export const accessToken = 'ad1c5e65e8c794862176b5057739f20bcde36343a9c55705984ca2a82a57f2a2';
+export const spaceId = 'hrunn6o3teke';
--- src/services/contentfulClient.js
+++ src/services/contentfulClient.js
@@ -1,104 +1,72 @@
 import { accessToken, spaceId } from '../../config';
+import { noImageAvailable } from '../helpers/images';
 import { createClient } from 'contentful';
-import Request from 'superagent';
 
 let client;
-// let authorized;
 
 export function initClient() {
-  client = client || createClient({
+  client = createClient({
     space: spaceId,
     accessToken,
     host: 'cdn.contentful.com'
   });
   return client.getSpace()
     .then((space) => {
-      // authorized = true;
       return space;
     });
 }
 
-// export function getClient() {
-//   return authorized && client;
-// }
-
-// export function loadProducts() {
-//   initClient();
-//   return client.getEntries({
-//     content_type: 'product'
-//   }).then((payload) => {
-//     console.log(payload.items);
-//     return payload.items;
-//   }).catch(console.error);
-// }
-
-
-
-
 export function loadProducts() {
   initClient();
-
-  // Request
-  //   .get('https://cdn.contentful.com/spaces/hrunn6o3teke/environments/master/entries')
-  //   .query({ 'content_type': 'product' })
-  //   .set('Authorization', `Bearer ${accessToken}`)
-  //   .then(({ body: { items } }) => {
-  //     // this.setState({ items });
-  //     console.log("---------");
-  //     console.log(items);
-  //     console.log("---------");
-  //     return items;
-  //   });
-
-
-
-  var result = [];
-
+  let result = [];
   return client.getEntries({
     content_type: 'product'
   }).then((payload) => {
-    // console.log(payload.items);
-    // return payload.items;
     payload.items.map((item) => {
-
       result.push({
         id: item.sys.id,
         title: item.fields.title,
         price: item.fields.price,
-        imageUrl: GetImageUrl(item)
+        imageUrl: GetPhoto(item)
       });
-      // result.map((it) => console.log(it));
     });
     return result;
   }).catch(console.error);
-  // // result.map((it) => console.log(it));
-  // console.log(typeof result);
-  // // return result;
 }
 
 
-// export function loadPhoto(id) {
-//   initClient();
-//   return client.getAsset(id).then((asset) => {
-//     return asset.fields;
-//   }).catch(console.error);
-// }
+export function loadProduct(id) {
+  initClient();
+  return client.getEntry(id).then((item) => {
+    return {
+      id: item.sys.id,
+      title: item.fields.title,
+      price: item.fields.price,
+      imageUrl: GetPhoto(item),
+      photos: GetPhotos(item)
+    };
+  }).catch(console.error);
+}
+
+function GetPhotos(item) {
+  let results = [];
+  let { photos } = item.fields;
+  photos.map((photo) =>
+    results.push(photoUrl(photo))
+  );
+  return results;
+}
 
 
-export function GetImageUrl(item) {
-  let imgUrl = 'https://www.vipspatel.com/wp-content/uploads/2017/04/no_image_available_300x300.jpg';
+function GetPhoto(item) {
+  let imgUrl = noImageAvailable();
   let { mainPhoto } = item.fields;
   if (mainPhoto != undefined) {
-    imgUrl = 'https:' + mainPhoto.fields.file.url;
+    imgUrl = photoUrl(mainPhoto);
   }
   return imgUrl;
+}
 
-  // else {
-  //   item.fields.imageUrl =
-  // }
-
-  // initClient();
-  // return client.getAsset(id).then((asset) => {
-  //   return asset.fields;
-  // }).catch(console.error);
+function photoUrl(photo) {
+  return 'https:' + photo.fields.file.url;
 }
